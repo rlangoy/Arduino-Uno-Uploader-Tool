f94a137d82926ca91fd8e00fa8bd866bcd27b4d7
Rune Langoy <rune.langoy@gmail.com>
2012-02-14 11:30:12 +0100

    The Intro form in it's own Windows.Form

---
+++ ArduinoUnoUploaderTool/FormIntro.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Windows.Forms;
+
+namespace HIVE.TEKMAR.ITEK.ArduinoUnoToolGui
+{
+    public partial class FormIntro : Form
+    {
+        public FormIntro()
+        {
+            InitializeComponent();
+        }
+    }
+}
